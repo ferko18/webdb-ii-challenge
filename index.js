63b0d66ee407a7421ceeaa0ad9105f5ba43dc658
Firealem Erko <firealemg@yahoo.com>
2019-06-04 15:47:23 -0400

    knex connected to db

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require("knex");
+
+const knexConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: "./data/lambda.db3"
+  },
+  useNullAsDefault: true
+};
+const db = knex(knexConfig);
 
 const server = express();
 
@@ -7,6 +17,10 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+//home endpoint 
+server.get("/", (req, res) => {
+  res.send(`<h2>Working!</h2>`);
+});
 
 const port = 3300;
 server.listen(port, function() {
